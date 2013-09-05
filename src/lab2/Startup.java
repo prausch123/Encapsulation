906a7bcd192807f9ec45acf648e178d99c2228e0
Peter <Peter@Peter-PC>
2013-09-05 13:49:13 -0500

    Created files for lab2

---
+++ src/lab2/Startup.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+/**
+ *
+ * @author Peter
+ */
+public class Startup {
+    
+    public static void main(String[] args) {
+        
+    }
+}
